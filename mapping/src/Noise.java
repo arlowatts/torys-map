92badb323e5b43fe3562e86fda1875667d1c63aa
Arlo <arlowatts1@gmail.com>
2022-08-01 14:17:13 -0700

    Cleaned Noise class

--- mapping/src/Noise.java
+++ mapping/src/Noise.java
@@ -39,43 +39,6 @@ public class Noise {
 					b % 1);
 	}
 	
-	/*public static double getNoise(int channel, double resolution, double x) {
-		x /= resolution;
-		
-		return lerp(random((long)x + randomLong(channel)),
-					random((long)x + randomLong(channel) + 1),
-					x % 1);
-	}
-	
-	public static double getNoise(int channel, double resolution, double width, double x, double y) {
-		y /= resolution;
-		
-		return lerp(getNoise(channel, resolution, x + (int)y * width),
-					getNoise(channel, resolution, x + ((int)y + 1) * width),
-					y % 1);
-	}
-	
-	public static double getNoise(int channel, double resolution, double width, double height, double x, double y, double z) {
-		z /= resolution;
-		
-		return lerp(getNoise(channel, resolution, width, x + (int)z * height * width, y),
-					getNoise(channel, resolution, width, x + ((int)z + 1) * height * width, y),
-					z % 1);
-	}*/
-	
-	/*private static double getNoise(int channel, double resolution, double x, int numDimensions, double[] dimensions, double[] coords) {
-		double prod = 1;
-		for (int i = 0; i < numDimensions - 1; i++) {
-			prod *= dimensions[i];
-		}
-		
-		return 0;
-	}
-	
-	public static double getNoise(int channel, double resolution, double[] dimensions, double... coords) {
-		return getNoise(channel, resolution, 0, dimensions.length, dimensions, coords);
-	}*/
-	
 	public static double lerp(double a, double b, double t) {
 		return a + t * (b - a);
 	}
