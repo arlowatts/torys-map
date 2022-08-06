06800960666df5977fbe1b56a5c7bf9d6dff7ecd
Arlo <arlowatts1@gmail.com>
2022-08-06 13:45:59 -0700

    Added some functionality to TorusMap.java

--- mapping/src/TorusMap.java
+++ mapping/src/TorusMap.java
@@ -1,27 +1,77 @@
 import java.util.ArrayList;
 
+import java.lang.Math;
+
 import java.awt.image.BufferedImage;
 
 public class TorusMap extends MapAbstract {
 	private double largeRadius, smallRadius;
 	
-	public double getAltitude(double x, double y);
+	public TorusMap(double R, double r) {
+		largeRadius = R;
+		smallRadius = r;
+		
+		zoom = 1;
+		currX = 0;
+		currY = 0;
+		
+		terrainNoise = new ArrayList<Noise>();
+		temperatureNoise = new ArrayList<Noise>();
+	}
 	
-	public double getWaterLevel(double x, double y);
+	public double getAltitude(double x, double y) {
+		double val = 0;
+		
+		double[] coords = getFullCoords(x, y);
+		
+		for (int i = 0; i < terrainNoise.size(); i++) {
+			val += terrainNoise.get(i).getNoise(coords);
+		}
+		
+		return Math.min(Math.max(val, 0), 1);
+	}
 	
-	public double getLight(double x, double y, double time);
+	public double getWaterLevel(double x, double y) {
+		return 0;
+	}
 	
-	public double getAverageLight(double x, double y);
+	public double getLight(double x, double y, double time) {
+		return 0;
+	}
 	
-	public double getTemperature(double x, double y, double time);
+	public double getAverageLight(double x, double y) {
+		return 0;
+	}
 	
-	public double getAverageTemperature(double x, double y);
+	public double getAverageLight(double x, double y, double startTime, double endTime) {
+		return 0;
+	}
 	
-	public ArrayList<Region> getRegions(double x, double y);
+	public double getTemperature(double x, double y, double time) {
+		return 0;
+	}
 	
-	public double[] getFullCoords(double x, double y);
+	public double getAverageTemperature(double x, double y) {
+		return 0;
+	}
 	
-	public double getX(double... coords);
+	public double getAverageTemperature(double x, double y, double startTime, double endTime) {
+		return 0;
+	}
 	
-	public double getY(double... coords);
+	public ArrayList<Region> getRegions(double x, double y) {
+		return new ArrayList<Region>();
+	}
+	
+	public double[] getFullCoords(double x, double y) {
+		return new double[] {0, 0};
+	}
+	
+	public double getX(double... coords) {
+		return 0;
+	}
+	
+	public double getY(double... coords) {
+		return 0;
+	}
 }
